ed30f71b9d8308f8ae6e3cceafb03ddfb8013cc0
Carjooj <victor@4info.com.br>
2023-09-23 19:37:46 -0300

    Projeto iniciado

---
+++ simus/src/main/java/com/pi/simus/SimusApplication.java
@@ -0,0 +1,13 @@
+package com.pi.simus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimusApplication.class, args);
+	}
+
+}
---
+++ simus/src/test/java/com/pi/simus/SimusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.simus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
