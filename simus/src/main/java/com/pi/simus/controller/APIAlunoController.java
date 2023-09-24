eab8352d70254f4527e5dd64b3c70cdce784838d
Carjooj <victor@4info.com.br>
2023-09-24 20:26:39 -0300

    Criação das rotas de POST, PUT, DELETE na classe de controle APIAlunoController

--- simus/src/main/java/com/pi/simus/controller/APIAlunoController.java
+++ simus/src/main/java/com/pi/simus/controller/APIAlunoController.java
@@ -6,12 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pi.simus.model.Aluno;
 import com.pi.simus.servico.IAlunoServico;
+
+import jakarta.transaction.Transactional;
 
 
 @RestController
@@ -36,6 +44,31 @@ public class APIAlunoController {
         logger.info("apicontroller consulta por cpf");
 
         return ResponseEntity.status(HttpStatus.OK).body(alunoServico.consultarPorCpf(cpf));
+    }
+
+    @CrossOrigin
+    @PostMapping("aluno")
+    public ResponseEntity<Object> cadastrarAluno(@RequestBody Aluno aluno) {
+        logger.info("apicontroller cadastro aluno");
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(alunoServico.cadastrarAluno(aluno));
+    }
+
+    @CrossOrigin
+    @PutMapping("aluno")
+    public ResponseEntity<Object> atualizarAluno(@RequestBody Aluno aluno) {
+        logger.info("apicontroller atualizar aluno");
+
+        return ResponseEntity.status(HttpStatus.OK).body(alunoServico.atualizarAluno(aluno));
+    }
+
+    @CrossOrigin
+    @DeleteMapping("aluno/{cpf}")
+    @Transactional
+    public ResponseEntity<Object> excluirAluno(@PathVariable String cpf) {
+        logger.info("apicontroller excluir aluno");
+
+        return ResponseEntity.status(HttpStatus.GONE).body(alunoServico.excluirAluno(cpf));
     }
     
 }
