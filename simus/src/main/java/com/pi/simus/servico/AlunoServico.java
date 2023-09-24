0e1b63ec50475a22f90107c6cd060f5ae32f27cd
Carjooj <victor@4info.com.br>
2023-09-24 20:25:41 -0300

    Implementação dos métodos de cadastrar, atualizar e excluir Aluno, na classe de serviço AlunoServico

--- simus/src/main/java/com/pi/simus/servico/AlunoServico.java
+++ simus/src/main/java/com/pi/simus/servico/AlunoServico.java
@@ -1,4 +1,5 @@
 package com.pi.simus.servico;
+
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -11,7 +12,6 @@ import org.springframework.stereotype.Service;
 import com.pi.simus.model.Aluno;
 import com.pi.simus.model.IAlunoRepository;
 
-
 @Service
 public class AlunoServico implements IAlunoServico {
 
@@ -21,11 +21,12 @@ public class AlunoServico implements IAlunoServico {
     IAlunoRepository alunoRepository;
 
     @Override
-    public Optional<Aluno> consultarPorCpf(String cpf) { 
+    public Optional<Aluno> consultarPorCpf(String cpf) {
+        logger.info("serviço consulta por cpf iniciado");
         Optional<Aluno> aluno = alunoRepository.findByCpf(cpf);
 
         return aluno;
-        
+
     }
 
     @Override
@@ -42,6 +43,7 @@ public class AlunoServico implements IAlunoServico {
 
     @Override
     public List<Aluno> consultaAluno() {
+        logger.info("serviço consulta todos iniciado");
         List<Aluno> alunos = alunoRepository.findAll();
 
         return alunos;
@@ -49,20 +51,36 @@ public class AlunoServico implements IAlunoServico {
 
     @Override
     public Optional<Aluno> cadastrarAluno(Aluno aluno) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'cadastrar'");
+        logger.info("serviço cadastrar aluno iniciado");
+
+        return Optional.ofNullable(alunoRepository.save(aluno));
     }
 
     @Override
-    public Optional<Aluno> atualizarAluno(Aluno aluno) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'atualizar'");
+    public Optional<Aluno> atualizarAluno(Aluno newAluno) {
+        logger.info("serviço atualizar aluno iniciado");
+
+        return alunoRepository.findByCpf(newAluno.getCpf())
+      .map(aluno -> {
+        aluno.setCep(newAluno.getCep());
+        aluno.setEstado(newAluno.getEstado());
+        aluno.setCidade(newAluno.getCidade());
+        aluno.setBairro(newAluno.getBairro());
+        aluno.setLogradouro(newAluno.getLogradouro());
+        aluno.setNumero(newAluno.getNumero());
+        aluno.setEmail(newAluno.getEmail());
+        aluno.setNomeCompleto(newAluno.getNomeCompleto());
+        aluno.setRg(newAluno.getRg());
+        aluno.setStatus(true);
+        return alunoRepository.save(aluno);
+      });
     }
 
     @Override
-    public void excluirAluno(Aluno aluno) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'excluir'");
+    public String excluirAluno(String cpf) {
+        logger.info("serviço excluir aluno iniciado");
+
+        return alunoRepository.deleteByCpf(cpf);
     }
-    
+
 }
