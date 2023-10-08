2f39a1c4af81748c187f91b9a840149286a5b855
Carjooj <victor@4info.com.br>
2023-10-08 13:42:42 -0300

    Interface Repository ProfessorRepository criada

---
+++ simus/src/main/java/com/pi/simus/model/IProfessorRepository.java
@@ -0,0 +1,12 @@
+package com.pi.simus.model;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+
+public interface IProfessorRepository extends JpaRepository<Professor, Long> {
+    public Optional<Professor> findByCpf(String cpf);
+    public Optional<Professor> findByNomeCompleto(String nomeCompleto);
+}
