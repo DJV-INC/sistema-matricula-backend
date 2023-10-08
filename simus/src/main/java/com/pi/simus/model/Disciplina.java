d20b3b66dc888e94410f919f36bbe0d01305441a
Carjooj <victor@4info.com.br>
2023-10-08 13:44:56 -0300

    Classe Model Disciplina criada

---
+++ simus/src/main/java/com/pi/simus/model/Disciplina.java
@@ -0,0 +1,42 @@
+package com.pi.simus.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Disciplina{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nome;
+
+    public Disciplina(){
+
+    }
+
+    public Disciplina(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+
+}
