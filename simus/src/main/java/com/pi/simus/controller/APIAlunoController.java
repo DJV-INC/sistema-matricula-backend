e89ac069ddfd167bd4d0a989ef4db49d71f6647f
Carjooj <victor@4info.com.br>
2023-10-09 15:34:13 -0300

    Classe de controle APIAlunoController Alteração no método de atualização Agora é utilizado PATCH ao invés de PUT

--- simus/src/main/java/com/pi/simus/controller/APIAlunoController.java
+++ simus/src/main/java/com/pi/simus/controller/APIAlunoController.java
@@ -10,9 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -82,7 +82,7 @@ public class APIAlunoController {
     }
 
     @CrossOrigin
-    @PutMapping("alunos")
+    @PatchMapping("alunos")
     @Transactional
     public ResponseEntity<Object> atualizarAluno(@RequestBody Aluno aluno) {
         logger.info("apicontroller atualizar aluno");
