551b32652baf861978be2ff809df722800a25833
Carjooj <victor@4info.com.br>
2023-10-14 19:25:48 -0300

    Preload de entidades Professor

--- simus/src/main/java/com/pi/simus/servico/LoadDatabase.java
+++ simus/src/main/java/com/pi/simus/servico/LoadDatabase.java
@@ -11,7 +11,8 @@ import org.springframework.context.annotation.Configuration;
 
 import com.pi.simus.model.Aluno;
 import com.pi.simus.model.IAlunoRepository;
-
+import com.pi.simus.model.IProfessorRepository;
+import com.pi.simus.model.Professor;
 import com.pi.simus.model.Aluno.StatusMatricula;
 
 @Configuration
@@ -21,8 +22,9 @@ public class LoadDatabase {
     @Autowired
     IAlunoRepository alunoRepository;
 
+
     @Bean
-    CommandLineRunner initDatabase(IAlunoRepository repository) {
+    CommandLineRunner initDatabase(IProfessorRepository professorRepository) {
         return args -> {
         Aluno aluno1 = new Aluno("12345678910", "580940305", "23/03/2003", "03562020", "SP", "São Paulo", "Vila Nhocune", "Rua Doutor João Priore", 102, "teste@gmail.com","11999615887", "Victor Cardoso Andrade");
 
@@ -33,7 +35,15 @@ public class LoadDatabase {
         aluno1.setStatusMatricula(StatusMatricula.ATIVA);
         aluno2.setStatusMatricula(StatusMatricula.ATIVA);
 
-        repository.saveAll(Arrays.asList(aluno1, aluno2, aluno3));
+        alunoRepository.saveAll(Arrays.asList(aluno1, aluno2, aluno3));
+
+        Professor professor1 = new Professor("Wilson Rezende", "54451318823", "11982942491");
+
+        Professor professor2 = new Professor("Jackeline Paula Teles Menezes", "10987654321", "11999615887");
+
+        Professor professor3 = new Professor("Paulo Domenech", "12345678910", "11927462417");
+
+        professorRepository.saveAll(Arrays.asList(professor1, professor2, professor3));
 
         logger.info("Loaddatabase -> aluno cadastrado");
         };
