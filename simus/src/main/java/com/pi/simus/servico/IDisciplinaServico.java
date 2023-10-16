62781463b4e10a9f9b6931c0ad7b77bd6ab1a0de
Carjooj <victor@4info.com.br>
2023-10-16 18:28:24 -0300

    Refatoração - Interface de serviço IDisciplinaServico
    Adicionado métodos consultaPorTipo, consultaPorId

--- simus/src/main/java/com/pi/simus/servico/IDisciplinaServico.java
+++ simus/src/main/java/com/pi/simus/servico/IDisciplinaServico.java
@@ -3,9 +3,12 @@ import java.util.Optional;
 
 import java.util.List;
 import com.pi.simus.model.Disciplina;
+import com.pi.simus.model.Disciplina.Tipo;
 
 public interface IDisciplinaServico{
     public List<Disciplina> consultaDisciplina();
+    public List<Disciplina> consultaPorTipo(Tipo tipo);
+    public Optional<Disciplina> consultaPorId(Long id);
     public Optional<Disciplina> consultarPorNome(String nome);
     public Optional<Disciplina> cadastrarDisciplina(Disciplina disciplina);
     public Optional<Disciplina> atualizarDisciplina(Disciplina disciplina);
