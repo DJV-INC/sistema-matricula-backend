077c165d4c987e37875481516920408d55304d4b
Carjooj <victor@4info.com.br>
2023-10-16 18:27:40 -0300

    Refatoração - Interface de Repositório IDisciplinaRepository
    Adicionado método findByTipo, removido método findById

--- simus/src/main/java/com/pi/simus/model/IDisciplinaRepository.java
+++ simus/src/main/java/com/pi/simus/model/IDisciplinaRepository.java
@@ -1,11 +1,15 @@
 package com.pi.simus.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pi.simus.model.Disciplina.Tipo;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface IDisciplinaRepository extends JpaRepository<Disciplina, Long>{
-    public Optional<Disciplina> findById(Long id);
     public Optional<Disciplina> findByNome(String nome);
+    public List<Disciplina> findByTipo(Tipo tipo);
     public void deleteById(Long id);
 }
 
