93e8bd53b9d4498a39f857f95771786784cfeb27
Carjooj <victor@4info.com.br>
2023-10-23 19:35:36 -0300

    Interface de serviço - ITurmaServico
    Criando classe e implementando

---
+++ simus/src/main/java/com/pi/simus/servico/ITurmaServico.java
@@ -0,0 +1,13 @@
+package com.pi.simus.servico;
+
+import java.util.Optional;
+import java.util.List;
+import com.pi.simus.model.Turma;
+import com.pi.simus.model.Disciplina;
+
+public interface ITurmaServico{
+    public List<Turma> consultaPorDisciplina(Optional<Disciplina> disciplina);
+    public Optional<Turma> cadastrarTurma(Turma turma);
+    public Optional<Turma> atualizarTurma(Turma turma);
+    public void excluirTurma(Long id);
+}
