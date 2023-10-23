e986d2cd66132ff5bc6980c9b3ffe53b190b5303
Carjooj <victor@4info.com.br>
2023-10-23 19:34:02 -0300

    Refatoração - Classes de Repositório
    Adicionando Anotação @Repository

--- simus/src/main/java/com/pi/simus/model/IDisciplinaRepository.java
+++ simus/src/main/java/com/pi/simus/model/IDisciplinaRepository.java
@@ -1,12 +1,14 @@
 package com.pi.simus.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.pi.simus.model.Disciplina.Tipo;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface IDisciplinaRepository extends JpaRepository<Disciplina, Long>{
     public Optional<Disciplina> findByNome(String nome);
     public List<Disciplina> findByTipo(Tipo tipo);
--- simus/src/main/java/com/pi/simus/model/IProfessorRepository.java
+++ simus/src/main/java/com/pi/simus/model/IProfessorRepository.java
@@ -3,9 +3,11 @@ package com.pi.simus.model;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
-
+@Repository
 public interface IProfessorRepository extends JpaRepository<Professor, Long> {
     public Optional<Professor> findByCpf(String cpf);
     public List<Professor> findByNomeCompleto(String nomeCompleto);
