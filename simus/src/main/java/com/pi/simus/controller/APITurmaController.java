5ac054363f7c60bae4244aed51cb1f37c687a1da
Carjooj <victor@4info.com.br>
2023-10-23 21:28:32 -0300

    Classe de controle - APITurmaController
    Endpoint POST cadastrarTurma implementado

--- simus/src/main/java/com/pi/simus/controller/APITurmaController.java
+++ simus/src/main/java/com/pi/simus/controller/APITurmaController.java
@@ -2,11 +2,14 @@ package com.pi.simus.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.hibernate.engine.transaction.jta.platform.internal.OC4JJtaPlatform;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +21,9 @@ import java.util.Optional;
 import com.pi.simus.model.Disciplina;
 import com.pi.simus.model.Turma;
 import com.pi.simus.model.ITurmaRepository;
+import com.pi.simus.model.Professor;
 import com.pi.simus.servico.IDisciplinaServico;
+import com.pi.simus.servico.IProfessorServico;
 import com.pi.simus.servico.ITurmaServico;
 
 import jakarta.transaction.Transactional;
@@ -35,6 +40,9 @@ public class APITurmaController {
     @Autowired
     IDisciplinaServico disciplinaServico;
 
+    @Autowired
+    IProfessorServico professorServico;
+
     @CrossOrigin
     @GetMapping(value = "turmas", params = "id")
     @Transactional
@@ -44,5 +52,25 @@ public class APITurmaController {
         List<Turma> turmas = turmaServico.consultaPorDisciplina(dis);
 
         return ResponseEntity.status(HttpStatus.OK).body(turmas);
+    }
+
+    @CrossOrigin
+    @PostMapping(value = "turmas")
+    @Transactional
+    public ResponseEntity<Object> cadastrarTurma(@RequestParam(value = "professor_id") Long professor_id, 
+    @RequestParam(value = "disciplina_id") Long disciplina_id, @RequestBody Turma turma) {
+
+        Optional<Professor> prof = professorServico.consultaPorId(professor_id);
+
+        Optional<Disciplina> dis = disciplinaServico.consultaPorId(disciplina_id);
+
+        turma.setProfessor(prof.get());
+
+        turma.setDisciplina(dis.get());
+
+        Optional<Turma> newTurma = turmaServico.cadastrarTurma(turma);
+
+        return ResponseEntity.status(HttpStatus.OK).body(newTurma);
+        
     }
 }
