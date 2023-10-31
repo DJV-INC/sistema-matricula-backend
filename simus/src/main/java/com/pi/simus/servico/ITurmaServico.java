6999ab3de31c559d17cc1863566412311c646758
Carjooj <victor@4info.com.br>
2023-10-30 23:44:45 -0300

    Interface de serviço - ITurmaServico
    Adicionado método de consulta por professor

--- simus/src/main/java/com/pi/simus/servico/ITurmaServico.java
+++ simus/src/main/java/com/pi/simus/servico/ITurmaServico.java
@@ -4,10 +4,12 @@ import java.util.Optional;
 import java.util.List;
 import com.pi.simus.model.Turma;
 import com.pi.simus.model.Disciplina;
+import com.pi.simus.model.Professor;
 
 public interface ITurmaServico{
     public Optional<Turma> consultaPorId(Long id);
     public List<Turma> consultaPorDisciplina(Optional<Disciplina> disciplina);
+    public List<Turma> consultaPorProfessor(Optional<Professor> professor);
     public Optional<Turma> cadastrarTurma(Turma turma);
     public Optional<Turma> atualizarTurma(Turma turma);
     public void excluirTurma(Long id);
